f5250121a12a86f2115f6721e2ad6f6b82e17c7f
maxwelljoslyn <maxwelljoslyn@gmail.com>
2016-03-06 22:57:35 +0800

    code to chunk the world up into regions with a kind of greedy fill algorithm thing. not guaranteed to be efficient, in fact I can almost guarantee the opposite; maybe I'll go read MIT Algos and do some fixing.

---
+++ Regions.py
@@ -0,0 +1,47 @@
+import random
+from Direction import Direction
+
+numRegions = 20
+dirNames = [n for n in Direction.__members__.keys()]
+
+def getRegionCoords(worldModel):
+    """Assigns a region (can be a country, a culture, whatever) to each HexData
+    stored as a value in the worldModel argument."""
+    # return value
+    # storage for coordinates belonging to each region
+    regionCoords = {x:[] for x in range(1,numRegions+1)}
+    unassignedCoords = list(worldModel.keys())
+    counter = 1
+    # assign starting coordinates for each region
+    while counter <= numRegions:
+        x = random.choice(unassignedCoords)
+        regionCoords[counter].append(x)
+        # maintain the property that validCoords only contains coords which are
+        # as yet unassigned to a region
+        validCoords.remove(x)
+    # assign all remaining coords by branching out from each start point
+    while validCoords != []:
+        for i in range(1,numRegions+1):
+            # select a coord in those already belonging to region i
+            # to serve as a "launch pad" from which to grab a neighboring coord
+            launchpad = random.choice(regionCoords[i])
+            # get its neighbors;
+            # we'll add as many as we can to the region
+            launchNeighs = worldModel[launchpad].neighbors.values()
+            # remove any neighbors which are invalid
+            # i.e. some region has already claimed them
+            validLaunchNeighs = [c for c in launchNeighs if c in validCoords)
+            # no valid launch neighs? oh well, I say. better luck next loop
+            for v in validLaunchNeighs:
+                regionCoords[i].append(v)
+                validCoords.remove(v)
+    # having gone through all valid coords, we're almost done
+    # first, though, we want to take the region label OFF water hexes
+    # regions are supposed to be confined to land only
+    for r,vals in regionCoords.items():
+        for v in vals:
+            if not worldModel[v].isLand:
+                vals.remove(v)
+    # all done, regions are nice and dry.
+    # now return the coords split up by region. voila!
+    return regionCoords
