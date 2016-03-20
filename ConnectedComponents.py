896086df882540b6b62c7957889aeae3e72093f9
maxwelljoslyn <maxwelljoslyn@gmail.com>
2016-03-20 17:37:58 +0800

    I want to write the code which determines how far it is from each market Q to each other market R that Q is connected to. this is straightforward shortest-path all-pairs calculation. however, the wrinkle is that not all markets are connected to all other markets. so there need to be multiple runs of the distance algorithm, one for each bundle of connected markets in roadModel. to slice up roadModel into said bundles, I wrote code to retrieve its connected components (graph theory term for a 'bundle'.)

---
+++ ConnectedComponents.py
@@ -0,0 +1,28 @@
+def DFS(source, num, components, visited):
+    """Depth-first search as helper function for getConnectedComponents."""
+    for dest in graph[source]:
+        if visited[dest] == False:
+            visited[dest] = True
+            components[num].append(dest)
+            DFS(dest, num, components, visited)
+
+def getConnectedComponents(graph):
+    """Argument is an nested-dict 'adjacency-list' representation of a graph.
+    Returns dictionary. Keys are integers; values are lists.
+    Each value-list contains the nodes of the graph which belong to that component."""
+    components = {}
+    numComponents = 0
+    visited = {g:False for g in graph}
+    for g in graph:
+        if visited[g] == False:
+            numComponents += 1
+            components[numComponents] = []
+            components[numComponents].append(g)
+            DFS(g, numComponents, components, visited)
+    for c,d in components.items():
+        d = list(set(d))
+        components[c] = d # don't know why just assigning to d isn't working, but oh well!
+    return(components)
+
+# sample graph
+graph = {'shrek': {'xyzzy': 1}, 'bar': {'baz': 1, 'foo': 1}, 'baz': {'bar': 1, 'foo': 1}, 'quux': {}, 'foo': {'bar': 1, 'baz': 1}, 'xyzzy': {'shrek': 1}}
