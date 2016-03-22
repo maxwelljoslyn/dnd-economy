8c79d8452c0ca5f4836220467b159744780e6a5d
maxwelljoslyn <maxwelljoslyn@gmail.com>
2016-03-22 20:10:33 +0800

    made naming nicer, since I'm planning to write a normal DFS that doesn't carry that extra info around

--- ConnectedComponents.py
+++ ConnectedComponents.py
@@ -1,10 +1,10 @@
-def DFS(source, graph, num, components, visited):
+def DFS_forComponents(source, graph, num, components, visited):
     """Depth-first search as helper function for getConnectedComponents."""
     for dest in graph[source]:
         if visited[dest] == False:
             visited[dest] = True
             components[num].append(dest)
-            DFS(dest, graph, num, components, visited)
+            DFS_forComponents(dest, graph, num, components, visited)
 
 def getConnectedComponents(graph):
     """Argument is an nested-dict 'adjacency-list' representation of a graph.
@@ -18,7 +18,7 @@ def getConnectedComponents(graph):
             numComponents += 1
             components[numComponents] = []
             components[numComponents].append(g)
-            DFS(g, graph, numComponents, components, visited)
+            DFS_forComponents(g, graph, numComponents, components, visited)
     for c,d in components.items():
         d = list(set(d))
         components[c] = d # don't know why just assigning to d isn't working, but oh well!
