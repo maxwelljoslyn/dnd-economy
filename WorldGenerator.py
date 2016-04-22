1d97d31815a3386dc8f2704ff67ce1b6f8156c99
maxwelljoslyn <maxwelljoslyn@gmail.com>
2016-04-22 19:29:47 +0800

    removed things from WorldGenerator which will be done by hand in this v3 of the system. changed mapsize to 40 temporarily for easier iteration cycles.

--- WorldGenerator.py
+++ WorldGenerator.py
@@ -18,7 +18,7 @@ random.seed(mySeed)
 getcontext().prec = 6
 
 # number of rings on the world map, with 0 = just the center hex
-mapSize = 80
+mapSize = 40
 
 # equatorial diameter of the planet
 # remember, I'm only generating one hemisphere right now
@@ -111,163 +111,6 @@ def tempAtCoord(coord):
     adjustment = Decimal(distanceFromCenter) * stepSize
     return(minTemp + adjustment)
 
-# todo: figure out anything to make this darn thing FASTER
-def buildFinalRoadModel(roadModel, worldModel):
-    """Creates the completely-connected road model, which has no disconnected
-    subgraphs, out of the basic road model, which has them."""
-    # disconnected sub-graphs of roadModelEcon
-    subgraphs = getConnectedComponents(roadModel)
-    while len(subgraphs) > 1:
-        print("debug sez: " + str(len(subgraphs)))
-        startPoints = subgraphs[1]
-        otherPoints = []
-        for i in range(2,len(subgraphs)):
-            otherPoints = otherPoints + subgraphs[i]
-        possibleTargets = []
-        for start in startPoints:
-            possibles = []
-            i = 2
-            while possibles == []:
-                nearbys = nearbyCoords(start,i)
-                # filter to get ones which actually contain markets
-                possibles = [n for n in nearbys if n in otherPoints]
-                i+=1
-            # find distances between start and all of these
-            pinfos = []
-            for p in possibles:
-                path,cost = AStarSearch(worldModel,start,p)
-                tup = (cost,path,p,start)
-                pinfos.append(tup)
-            pinfos.sort()
-            # take the nearest of targets reachable from this start
-            possibleTargets.append(pinfos[0])
-        # take the nearest of all near targets
-        possibleTargets.sort()
-        target = possibleTargets[0]
-        targetSpecificStart = target[3]
-        targetCoord = target[2]
-        targetCost = target[0]
-        targetPath = target[1]
-        insertIntoRoadModel(targetSpecificStart,targetCoord,(targetCost,targetPath),roadModel)
-        # re-get the subgraphs
-        subgraphs = getConnectedComponents(roadModel)
-        print("debug: subgraphs going down!")
-        # thus a link is forged between two subgraphs,
-        # lowering the total number thereof by one.
-    return roadModel
-
-def insertIntoRoadModel(begin,end,info, roadModel):
-        """Puts two connections into the roadmodel:
-        one from begin to end, one from end to begin.
-        Note that it does not handle removing entries from uncheckedCoords!
-        Note also that this fun manipulates outer state by editing roadModelR.
-        Info is a tuple of distance and the path of that distance between begin and end."""
-        #membership test: has an entry for either coord
-        # already been created in the road model,
-        # because another market already connects to it?
-        if begin not in roadModel:
-            roadModel[begin] = {}
-        if end not in roadModel:
-            roadModel[end] = {}
-        roadModel[begin][end] = info
-        roadModel[end][begin] = info
-
-def buildBasicRoadModel(marketModel, worldModel):
-    """Build up the road model from the locations of markets and elevation/distance info
-    from the world model."""
-    roadModelR = {}
-    uncheckedCoords = list(marketModel.keys())
-    # first pass
-    # attach any markets with neighboring markets at LINEAR distance 1,
-    # to all such markets
-    print("debug entering THE ZONE")
-    print(uncheckedCoords)
-    for c in uncheckedCoords:
-        veryCloseMarkets = getMarketsWithinLinearDist(c, 1, marketModel,
-                                                   worldModel, roadModelR)
-        if veryCloseMarkets == []:
-            pass
-        else:
-            for v in veryCloseMarkets:
-                path,cost = AStarSearch(worldModel, c, v)
-                info = (cost,path)
-                insertIntoRoadModel(c,v,info,roadModelR)
-                # we don't remove v: it might yet have other connections to be made!
-                print("debug: removing " + str(c) + " from unchecked; first pass")
-            uncheckedCoords.remove(c)
-    # subsequent passes
-    # increase the acceptable linear distance from a market to a
-    # candidate target market.
-    dist = 2
-    while uncheckedCoords != []:
-        print("entering subsequent pass with distance " + str(dist))
-        print("remaining unchecked: " + str(len(uncheckedCoords)))
-        for c in uncheckedCoords:
-            kindaCloseMarkets = getMarketsWithinLinearDist(c, dist, marketModel,
-                                                   worldModel, roadModelR)
-            # IMPORTANT: each candidate market must already be part of the roadmodel:
-            # this is what creates a graph with no disconnected subgraphs.
-            # edit: this totally doesn't work, because there's no guarantee that a
-            # coord removed in the first pass will necessarily be linking up to others,
-            # meaning a later coord linking to that first-pass removal, will also
-            # not have any link to the greater system.
-            kindaCloseMarkets = [k for k in kindaCloseMarkets if k not in uncheckedCoords]
-            if kindaCloseMarkets == []:
-                pass
-            else:
-                # on each candidate run AStar
-                infoDict = {k:AStarSearch(worldModel, c, k) for k in kindaCloseMarkets}
-                # swap position of path and cost so they can easily be sorted by cost
-                for candidate,(path,cost) in infoDict.items():
-                    infoDict[candidate] = (cost,path)
-                # setting a "default" target which is expected to be beaten
-                target = list(infoDict)[0]
-                targetInfo = infoDict[candidate]
-                for candidate,(cost,path) in infoDict.items():
-                    if cost < targetInfo[0]:
-                        target = candidate
-                        targetInfo = (cost,path)
-                    else:
-                        pass
-                insertIntoRoadModel(c,target,targetInfo,roadModelR)
-                # then remove coord from unchecked
-                print("debug: removing " + str(c) + " from unchecked; sub pass " + str(dist))
-                uncheckedCoords.remove(c)
-        # increase acceptable linear distance at which to "fish" for markets, and go again
-        dist+=1
-    return roadModelR
-
-def getMarketsWithinLinearDist(coord, dist, marketModel, worldModel, roadModel):
-    """Returns all, if any, coords within linear dist of dist from coord,
-    which are market-having coords."""
-    hs = nearbyHexes(coord, dist)
-    marketHavers = [m for m in list(marketModel.keys()) for h in hs if m == h]
-    for m in marketHavers:
-        if m in roadModel:
-            if coord in roadModel[m]:
-                # coord in m dict
-                marketHavers.remove(m)
-            elif coord in roadModel:
-              if m in roadModel[coord]:
-                # m in coord dict
-                marketHavers.remove(m)
-    return marketHavers
-    
-# this is v1
-# one day there will be a version which has a different name gen scheme
-# for each of several regions defined on the map
-def makeMarketName():
-    numSoundPairs = random.randint(2,6)
-    vowels = ["a","e","i","o","u"]
-    cons = ["b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v","w","z"]
-    result = []
-    # starting consonant is made uppercase
-    result.append(str.upper(random.choice(cons)))
-    while numSoundPairs > 0:
-        result.append(random.choice(vowels))
-        result.append(random.choice(cons))
-        numSoundPairs -= 1
-    return "".join(result)
 
 # TODO: write my own noise generator (see AmitP noise page)
 def initialize():
@@ -610,69 +453,11 @@ def initialize():
 
                 chanceResourceCount -= 1
 
-    # now, before making markets, we assign a region to each land hex
-    # SET THE RANDOM SEED
-#    random.seed(mySeed)
-#    regionAssignments = Regions.getRegionCoords(worldModel)
-#    for r,vals in regionAssignments.items():
-#        for v in vals:
-#            data = worldModel[v]
-#            if data.isLand == False:
-#                data.region = 0
-#            else:
-#                data.region = r
-                
-    # creating cities in some, but not all, hexes, and giving hex resources to them
-    # SET THE RANDOM SEED
-    random.seed(mySeed)
-    marketModel = {}
-    for coord,data in worldModel.items():
-        resCount = sum(list(data.resources.values()))
-        # market chance:
-        # base chance of having a market is based on num of resources.
-        # a resource is counted multiple times if there are multiple abstract units of it
-        chanceOfMarket = sum(list(data.resources.values())) * 5
-        x = random.randint(1,100)
-        if x <= chanceOfMarket:
-            n = makeMarketName() # todo: change this to be region-specific
-            # this while loop prevents duplication of names
-            # it gets slower as you add more cities since they all have to be checked
-            while n in marketModel:
-                print(n,"is already used.")
-                n = makeMarketName()
-                print("now it's:",n)
-            marketModel[coord] = n
-        else:
-            pass
+    return worldModel
 
-    # holds the network of roads spanning the markets,
-    # keyed by COORD (used for the map renderer)
-    # after building this, we'll build a version which is keyed by name,
-    # in order to do shortest-path calculation for the economic system
-    roadModelRender = buildFinalRoadModel(buildBasicRoadModel(marketModel,worldModel), worldModel)
-
-    # roadModelRender is:
-    # dict (coord1 -> dict (coord2 -> (distance, path from coord2 to coord1, expressed in coords)))
-    # we want to make a second version of roadModelRender, called roadModelEcon,
-    # in which coord1 and coord2 are changed to the market names at those coords.
-    # then this second version can be sent off to the econ-calculator programs.
-    roadModelEcon = {}
-    for c1,valdict in roadModelRender.items():
-        name1 = marketModel[c1]
-        roadModelEcon[name1] = {}
-        for c2,info in valdict.items():
-            name2 = marketModel[c2]
-            roadModelEcon[name1][name2] = info
-
-    return (worldModel, marketModel, roadModelEcon, roadModelRender)
-
-# creating and using the above definitions/functions
-worldModelReady, marketModelReady, roadModelEconReady, roadModelRenderReady = initialize()
-
-subgraphs = getConnectedComponents(roadModelRenderReady)
+worldModelReady = initialize()
 
 def main():
-    print(subgraphs)
     counter = {}
     with open("inputWorldParser.txt", "w") as f:
         for c,d in worldModelReady.items():
@@ -692,35 +477,6 @@ def main():
                     counter[x] = 1
     totalWorldCount = sorted([(x,y) for (y,x) in counter.items()])
     print(totalWorldCount)
-
-    print("\n\n\n")
-    # count up those resources which ARE available through a market
-    accumulator = {}
-    for c,n in marketModelReady.items():
-        reses = worldModelReady[c].resources
-        for r,count in reses.items():
-            if r in accumulator:
-                accumulator[r] += count
-            else:
-                accumulator[r] = 1
-    totalAccumulator = sorted([(x,y) for (y,x) in accumulator.items()])
-    print(totalAccumulator)
-
-    with open("inputMarketParser.txt", "w") as f:
-        for c,n in marketModelReady.items():
-            outputString = "Coord " + str(c) + " Name " + n + "\n"
-            f.write(outputString)
-
-    with open("inputRoadParser.txt", "w") as f:
-        for coord, targets in roadModelRenderReady.items():
-            for target, data in targets.items():
-                distance = data[0]
-                path = data[1]
-                pathString = ",".join([("Coord " + str(x)) for x in path])
-                outputString = "[" + pathString + "]\n"
-                f.write(outputString)
-
-    
 
 if __name__ == "__main__":
     main()
