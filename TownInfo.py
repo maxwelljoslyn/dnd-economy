6b83144166490e4cf8c98b89d8430430b431af44
maxwelljoslyn <maxwelljoslyn@gmail.com>
2016-06-25 13:06:51 -0700

    added towns and adjusted some too

--- TownInfo.py
+++ TownInfo.py
@@ -37,7 +37,7 @@ towns["Gootonu"] = Town((43,-66,23),
                         {"smelter":2, "baker":1, "brewer":1, "tanner":1})
 towns["Torkada"] = Town((42,-67,25),
                         {"arable land":1, "clay":1, "cereal":2},
-                        {"butcher":1,"farmer":1, "potter":1, "miller":1})
+                        {"butcher":1,"potter":1, "dyer":1, "miller":1})
 towns["Serdabach"] = Town((45,-68,23),
                           {"salt":1, "arable land":1, "timber":2},
                           {"smelter":1, "farmer":1, "carpenter":1, "brewer":1})
@@ -45,18 +45,31 @@ towns["Berdorl"] = Town((43,-69,26),
                         {"salt":1,"coal":1, "iron ore":1, "limestone":1},
                         {"potter":1, "tanner":1, "blacksmith":1})
 towns["Kededal"] = Town((43,-60,17),
-                        {"arable land":2},
-                        {"vintner":3})
+                        {"arable land":2, "hops":1},
+                        {"vintner":1})
 towns["Yog"] = Town((45,-60,15),
                     {"arable land":1, "limestone":1},
-                    {"farmer":1, "tanner":2, "leatherworker":1})
+                    {"farmer":1, "tanner":1, "brewer":1, "leatherworker":1})
 towns["Dreot"] = Town((47,-67,20),
                       {"arable land":2, "timber":1, "cereal":1},
-                      {"miller":1, "potter":1})
-towns["Guroff"]=Town((47,-62,15),
-                     {"clay":1, "gold ore":2, "silver ore":1, "manganese ore":1, "nickel ore":1},
+                      {"miller":1, "carpenter":1})
+towns["Guroff"]=Town((47,-63,16),
+                     {"clay":1, "gold ore":2, "silver ore":1, "nickel ore":1},
                      {"goldsmith":1, "silversmith":1, "smelter":1})
+towns["Nender"] = Town((48,-66,18),
+                       {"lapis lazuli":1,"manganese ore":1},
+                       {"potter":1})
+towns["Pwodd"] = Town((49,-65,16),
+                      {"arable land":1,"cereal":1, "hops":1},
+                      {"tailor":1, "baker":1})
+towns["Widder"] = Town((49,-63,14),
+                       {"arable land":2},
+                       {"tailor":1,"weaver":1,"butcher":1})
 
+addConnection("Pwodd","Widder")
+addConnection("Widder","Guroff")
+addConnection("Nender","Pwodd")
+addConnection("Dreot","Nender")
 addConnection("Yog","Guroff")
 addConnection("Dreot","Ekodo")
 addConnection("Yog","Kededal")
