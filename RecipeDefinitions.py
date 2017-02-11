fb9ecd1da534a6ce3c82b102ee6a0d5588ac3738
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2017-02-10 19:38:19 -0800

    added density of gold

--- RecipeDefinitions.py
+++ RecipeDefinitions.py
@@ -54,6 +54,7 @@ densityLead = 709
 percentageClayInGlass = Decimal(0.8)
 densityMercury = Decimal(844.900018) # in liquid form
 densityHorn = Decimal(81.1563)
+densityGold = Decimal(1204.86)
 
 cubicFootInPints = Decimal(59.8442)
 def cylinderCuFt(height,radius):
